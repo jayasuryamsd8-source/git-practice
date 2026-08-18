c062ad3b2979b2387c9aa38abf157a9621abc342
Your Name <https://github.com/jayasuryaNew/Netflix.git>
2026-08-18 10:13:54 +0530

    Create Project

---
+++ script.js
@@ -0,0 +1,15 @@
+function addTask() {
+    const input = document.getElementById("taskInput");
+    const taskList = document.getElementById("taskList");
+
+    if (input.value === "") {
+        return;
+    }
+
+    const li = document.createElement("li");
+    li.textContent = input.value;
+
+    taskList.appendChild(li);
+
+    input.value = "";
+}
